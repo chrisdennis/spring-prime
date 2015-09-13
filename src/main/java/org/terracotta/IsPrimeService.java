318022eecea2610fe2b414f26ca125d55c8cc215
Chris Dennis <chris.w.dennis@gmail.com>
2015-09-13 10:26:44 -0400

    add caching annotations

--- src/main/java/org/terracotta/IsPrimeService.java
+++ src/main/java/org/terracotta/IsPrimeService.java
@@ -1,12 +1,17 @@
 package org.terracotta;
 
+import javax.cache.annotation.CacheResult;
+import javax.cache.annotation.CacheDefaults;
+
 import org.springframework.stereotype.Component;
 
 import static java.util.stream.LongStream.range;
 
 @Component
+@CacheDefaults(cacheName = "is-prime")
 public class IsPrimeService {
 
+  @CacheResult
   public boolean isPrime(long number) {
     if (number <= 0) {
       throw new IllegalArgumentException();
