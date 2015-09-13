8378378069cf4dc01a75a3f7ee56c08346d588d8
Chris Dennis <chris.w.dennis@gmail.com>
2015-09-13 10:26:44 -0400

    add caching annotations

--- src/main/java/org/terracotta/NthPrimeService.java
+++ src/main/java/org/terracotta/NthPrimeService.java
@@ -1,16 +1,20 @@
 package org.terracotta;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.cache.annotation.CacheDefaults;
+import javax.cache.annotation.CacheResult;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static java.util.stream.LongStream.range;
 
 @Component
+@CacheDefaults(cacheName = "nth-prime")
 public class NthPrimeService {
 
   @Autowired IsPrimeService isPrimeService;
   
+  @CacheResult
   public long primeNumber(long index) {
     return range(1, Long.MAX_VALUE).filter(isPrimeService::isPrime).limit(index).reduce(-1L, (a, b) -> b);
   }
